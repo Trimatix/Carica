cd7dd807a0c6570981b30a2117e701ab8425ccc1
Trimatix <1jasperlaw@gmail.com>
2021-09-06 21:22:11 +0100

    make ISerializable a runtime checkable protocol

--- src/carica/interface/ISerializable.py
+++ src/carica/interface/ISerializable.py
@@ -1,27 +1,25 @@
 from __future__ import annotations
-from abc import ABC, abstractmethod
-from typing import Dict, List, Set, Union
+from typing import Dict, List, Set, Union, Protocol, runtime_checkable
 
 PrimativeType = Union[int, float, str, bool, List["PrimativeType"], Dict["PrimativeType", "PrimativeType"], Set["PrimativeType"]] # type: ignore
 
 
-class ISerializable(ABC):
+@runtime_checkable
+class ISerializable(Protocol):
     """An object which can be represented entirely by a dictionary of primitives, created with the toDict method.
     This object can then be recreated perfectly using the fromDict method.
     """
     
-    @abstractmethod
     def serialize(self, **kwargs) -> PrimativeType:
         """Serialize this object into primative types (likely a dictionary, e.g JSON), to be recreated completely.
 
         :return: A primative (likely a dictionary) containing all information needed to recreate this object
         :rtype: PrimativeType
         """
-        raise NotImplementedError()
+        ...
 
 
     @classmethod
-    @abstractmethod
     def deserialize(cls, data: PrimativeType, **kwargs) -> ISerializable:
         """Recreate a serialized ISerializable object
 
@@ -29,4 +27,4 @@ class ISerializable(ABC):
         :return: A new object as specified by data
         :rtype: ISerializable
         """
-        raise NotImplementedError()
+        ...
