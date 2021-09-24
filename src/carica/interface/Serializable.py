0d94a128ac9e79bcd4b2d8584c5f1e45ae98f5c4
Trimatix <1jasperlaw@gmail.com>
2021-09-24 23:46:03 +0100

    type checking things have moved to the typeChecking module

--- src/carica/interface/Serializable.py
+++ src/carica/interface/Serializable.py
@@ -1,66 +1,12 @@
 from __future__ import annotations
 from abc import ABC, abstractmethod
 from typing import Any, Dict, List, Set, Union, Protocol, runtime_checkable
+from carica import exceptions
 
 # All types acceptable as toml data
 PrimativeType = Union[int, float, str, bool, List["PrimativeType"], Dict[str, "PrimativeType"], Set["PrimativeType"]] # type: ignore
 # PrimativeType as a shallow set
 primativeTypes = {int, float, str, bool, list, dict, set}
-
-
-def objectIsShallowPrimative(o: Any) -> bool:
-    """Decide if an object is a primative type, excluding any objects potentially contained within it.
-    I.e:
-    ```
-    >>> objectIsShallowPrimative(1)
-    True
-    >>> class MyNonPrimativeClass:
-    ...    pass
-    ...
-    >>> objectIsShallowPrimative(MyNonPrimativeClass())
-    False
-    >>> objectIsShallowPrimative([MyNonPrimativeClass()])
-    True
-    ```
-
-    :param Any o: The object whose type to check
-    :return: True if o itself is a primative type, False otherwise
-    :rtype: bool
-    """
-    return any(isinstance(o, t) for t in primativeTypes)
-
-
-def objectIsDeepPrimative(o: Any) -> bool:
-    """Decide if a class is a primative type, including any objects potentially contained within it.
-    I.e:
-    ```
-    >>> objectIsDeepPrimative(1)
-    True
-    >>> objectIsDeepPrimative([1])
-    True
-    >>> class MyNonPrimativeClass:
-    ...    pass
-    ...
-    >>> objectIsDeepPrimative(MyNonPrimativeClass())
-    False
-    >>> objectIsDeepPrimative([MyNonPrimativeClass()])
-    False
-    ```
-
-    This process is highly inefficient, as it recurses through all member objects in `o`.
-    For a more efficient (but less confident) check, use `objectIsShallowPrimative` to check if just `o` itself is primative,
-    excluding any member objects.
-
-    :param Any o: The object whose type to check
-    :return: True if o itself is a primative type, False otherwise
-    :rtype: bool
-    """
-    if isinstance(o, list) or isinstance(o, set):
-        return all(objectIsDeepPrimative(i) for i in o)
-    elif isinstance(o, dict):
-        return all(isinstance(k, str) and objectIsDeepPrimative(v) for k, v in o.items())
-    else:
-        return objectIsShallowPrimative(o)
 
 
 @runtime_checkable
@@ -116,79 +62,8 @@ class ISerializable(ABC):
         :rtype: ISerializable
         """
         raise NotImplementedError()
-
+    
 
 # All types which are themselves primative, or can be serialized into primative types, as a shallow set
 serializableTypes = primativeTypes.copy()
 serializableTypes.add(SerializableType)
-
-
-def objectIsShallowSerializable(o: Any) -> bool:
-    """Decide if an object is a primative or serializable type, excluding any objects potentially contained within it.
-    I.e:
-    ```
-    >>> objectIsShallowSerializable(1)
-    True
-    >>> class MySerializableClass:
-    ...    def serialize(self, **kwargs):
-    ...        return {}
-    ...    def deserialize(self, data **kwargs):
-    ...        return MySerializableClass()
-    ...
-    >>> objectIsShallowSerializable(MySerializableClass())
-    True
-    >>> class MyNonPrimativeClass:
-    ...    pass
-    ...
-    >>> objectIsShallowSerializable(MyNonPrimativeClass())
-    False
-    >>> objectIsShallowSerializable([MyNonPrimativeClass()])
-    True
-    ```
-
-    :param Any o: The object whose type to check
-    :return: True if o itself is a primative or serializable type, False otherwise
-    :rtype: bool
-    """
-    return any(isinstance(o, t) for t in serializableTypes)
-
-
-def objectIsDeepSerializable(o: Any) -> bool:
-    """Decide if a class is a primative or serializable type, including any objects potentially contained within it.
-    I.e:
-    ```
-    >>> objectIsDeepSerializable(1)
-    True
-    >>> class MySerializableClass:
-    ...    def serialize(self, **kwargs):
-    ...        return {}
-    ...    def deserialize(self, data **kwargs):
-    ...        return MySerializableClass()
-    ...
-    >>> objectIsDeepSerializable(MySerializableClass())
-    True
-    >>> objectIsDeepSerializable([MySerializableClass()])
-    True
-    >>> class MyNonPrimativeClass:
-    ...    pass
-    ...
-    >>> objectIsDeepSerializable(MyNonPrimativeClass())
-    False
-    >>> objectIsDeepSerializable([MyNonPrimativeClass()])
-    False
-    ```
-
-    This process is highly inefficient, as it recurses through all member objects in `o`.
-    For a more efficient (but less confident) check, use `objectIsShallowPrimative` to check if just `o` itself is primative,
-    excluding any member objects.
-
-    :param Any o: The object whose type to check
-    :return: True if o itself is a primative type, False otherwise
-    :rtype: bool
-    """
-    if isinstance(o, list) or isinstance(o, set):
-        return all(objectIsDeepSerializable(i) for i in o)
-    elif isinstance(o, dict):
-        return all(isinstance(k, str) and objectIsDeepSerializable(v) for k, v in o.items())
-    else:
-        return objectIsShallowSerializable(o)
