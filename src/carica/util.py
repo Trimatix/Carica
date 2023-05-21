32b093a05e5bd4cf73eac4bed894f8058dc4ae4f
Trimatix <1JasperLaw@gmail.com>
2023-05-21 21:06:36 +0100

    unwrap tomlkit array types

--- src/carica/util.py
+++ src/carica/util.py
@@ -1,10 +1,10 @@
 from typing import Any, Dict, List, Union, cast
 from tomlkit.container import Container as TKContainer
-from tomlkit.items import AoT, Table
+from tomlkit.items import Array, AoT, Table
 
-INCOMPATIBLE_TOML_TYPES = (TKContainer, AoT, Table)
+INCOMPATIBLE_TOML_TYPES = (TKContainer, Table, Array, AoT)
 
-def convertIncompatibleTomlTypes(doc: Union[TKContainer, AoT, Table]) -> Union[List[Any], Dict[str, Any]]:
+def convertIncompatibleTomlTypes(doc: Union[TKContainer, AoT, Array, Table]) -> Union[List[Any], Dict[str, Any]]:
     """Recursively converts tomlkit tables/lists of tables into dictionaries/lists of dictionaries
 
     :param doc: The item to convert
@@ -14,7 +14,7 @@ def convertIncompatibleTomlTypes(doc: Union[TKContainer, AoT, Table]) -> Union[L
     if isinstance(doc, (TKContainer, Table)):
         # Not sure why mypy doesn't pick up on Container inheriting from dict
         return {k: convertIncompatibleTomlTypes(v) for k, v in  cast(dict, doc).items()}
-    elif isinstance(doc, AoT):
+    elif isinstance(doc, (Array, AoT)):
         return [convertIncompatibleTomlTypes(v) for v in doc]
     else:
         return doc
